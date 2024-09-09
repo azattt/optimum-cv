a0556a2981e6365b27b271def29790b97e04ac90
azattt <azat.zad@bk.ru>
2024-09-10 01:35:26 +0300

    end

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 import cv2
 import numpy as np
@@ -34,14 +35,11 @@ if USE_TESSERACT:
                 )
         return out
 
-
-# проблема с download3.png
-image = cv2.imread("images/map_samples/download2.png")
-image_height = image.shape[0]
-image_width = image.shape[1]
-
 MIN_PERIMETER = 10
 MIN_AREA = 300
+SHOW_FIND_BIG_AREAS_STAGES = True
+if SHOW_FIND_BIG_AREAS_STAGES:
+    os.makedirs("results/find_big_areas_v1", exist_ok=True)
 
 def find_big_areas(src: np.ndarray) -> list[tuple[np.ndarray, np.ndarray]]:
     """Эта функция ищет кадастровые кварталы (или как я их назвал большие участки)
@@ -50,6 +48,7 @@ def find_big_areas(src: np.ndarray) -> list[tuple[np.ndarray, np.ndarray]]:
     Для этого изображения преобразовываетя в HSV и убираются все цвета, кроме красного
     Этот метод не идеален, например для images/map_samples/download3.png функция не может выделить участки,
     из-за того, что не хватает буквального 1 пикселя для замыкания контура
+    Как работает первая версия алгоритма есть в папке images/results/find_big_areas_v1
     """
     out: list[tuple[np.ndarray, np.ndarray]] = []
     src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
@@ -58,11 +57,15 @@ def find_big_areas(src: np.ndarray) -> list[tuple[np.ndarray, np.ndarray]]:
     ), cv2.inRange(
         src_hsv, np.array((177, 110, 0), dtype=np.uint8), np.array((179, 255, 176), dtype=np.uint8)
     ))
-
+    if SHOW_FIND_BIG_AREAS_STAGES:
+        cv2.imwrite("images/results/find_big_areas_v1/threshold.png", threshold)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     removed_blobs = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+    if SHOW_FIND_BIG_AREAS_STAGES:
+        cv2.imwrite("images/results/find_big_areas_v1/removed_blobs.png", removed_blobs)
     contours, _ = cv2.findContours(removed_blobs, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+    i = 0
     for cnt in contours:
         if cv2.arcLength(cnt, True) < MIN_PERIMETER:
             continue
@@ -73,20 +76,32 @@ def find_big_areas(src: np.ndarray) -> list[tuple[np.ndarray, np.ndarray]]:
 
         contour_mask = np.zeros(src.shape[:-1], dtype=np.uint8)
         cv2.fillPoly(contour_mask, [cnt], (255, ))
-
+        if SHOW_FIND_BIG_AREAS_STAGES:
+            cv2.imwrite(f"images/results/find_big_areas_v1/removed_blobs_{i}.png", removed_blobs)
         x, y, w, h = cv2.boundingRect(cnt)
         cropped = cv2.bitwise_and(src, src, mask=contour_mask)[y : y + h, x : x + w]
+        if SHOW_FIND_BIG_AREAS_STAGES:
+            cv2.imwrite(f"images/results/find_big_areas_v1/cropped_{i}.png", cropped)
         out.append((cropped, (cv2.bitwise_and(threshold, contour_mask))[y : y + h, x : x + w]))
-
+        i += 1
+        
     return out
 
 def divide_big_area_to_smaller(big_area: tuple[np.ndarray, np.ndarray]) -> Any:
     """Эта функция должна находить участки внутри кадастрового квартала"""
     pass
 
-areas = find_big_areas(image)
-for i, images in enumerate(areas):
-    cv2.imshow(f"cropped_{i}", images[0])
-    cv2.imshow(f"threshold_{i}", images[1])
+def main():
+    # проблема с download3.png
+    image = cv2.imread("images/map_samples/download2.png")
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+    areas = find_big_areas(image)
+    for i, images in enumerate(areas):
+        cv2.imshow(f"cropped_{i}.png", images[0])
+        cv2.imshow(f"threshold_{i}.png", images[1])
 
-cv2.waitKey()
+    cv2.waitKey()
+
+if __name__ == "__main__":
+    main()
