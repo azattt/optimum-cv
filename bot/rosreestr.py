0e6a02b2e06aa9c0fae732fdef5a326957b69564
azattt <azat.zad@bk.ru>
2024-09-24 22:47:58 +0300

    и

--- bot/rosreestr.py
+++ bot/rosreestr.py
@@ -1,10 +1,30 @@
+from json import JSONDecodeError
+import logging
 import time
-from urllib.parse import urlencode
 
 from curl_cffi import requests
 
-def search_locality(text: str):
-    r = requests.get("https://pkk.rosreestr.ru/api/typeahead/1", params={"text": text, "_": int(time.time()*1000)}, impersonate="chrome110", verify="russian_trusted_root_ca.cer")
+logger = logging.getLogger("rosreestr")
+
+async def search_address(text: str, tolerance: int = 32768) -> dict | None:
+    raise RuntimeError()
+    async with requests.AsyncSession() as s:
+        r = s.get(
+            "https://pkk.rosreestr.ru/arcgis/rest/services/Address/pkk_locator_street/GeocodeServer/findAddressCandidates",
+            params={"SingleLine": text, "f": "json", "tolerancy": tolerance},
+            impersonate="chrome110",
+            verify="russian_trusted_root_ca.cer",  # type: ignore[arg-type]
+        )
+        data: dict = r.json()
+        return data.get("candidates", None)
+
+
+async def search_locality(text: str):
+    r = requests.get(
+        "https://pkk.rosreestr.ru/api/typeahead/1",
+        params={"text": text, "_": int(time.time() * 1000), "tolerancy": 0},
+        impersonate="chrome110",
+        verify="russian_trusted_root_ca.cer",  # type: ignore[arg-type]
+    )
     print(r.text)
 
-search_locality("пиголи")
--- bot/user_router.py
+++ bot/user_router.py
@@ -1,3 +1,4 @@
+import logging
 from aiogram import Router, F, Bot
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
@@ -5,16 +6,28 @@ from aiogram.filters import CommandStart
 
 from states import UserStates
 
-from rosreestr import search_locality
+from rosreestr import search_address
 
+logger = logging.getLogger("user_router")
 user_router = Router()
 
 @user_router.message(CommandStart())
+@user_router.message(UserStates.start)
 async def start_message(message: Message, state: FSMContext):
     await message.answer("Введите адрес населенного пункта")
     await state.set_state(UserStates.search_input)
 
 @user_router.message(UserStates.search_input)
 async def search_input(message: Message, state: FSMContext):
-    
-    await message.answer("Результаты поиска")
+    if message.text is None:
+        await message.answer("Неверный адрес.")
+        return
+    try:
+        result = await search_address(message.text)
+    except Exception:
+        message.answer("Ошибка поиска. Отчет об ошибке отправлен")
+        await state.set_state(UserStates.start)
+        await user_router.propagate_event("message", message)
+        raise
+    if result is None:
+        await message.answer("Результаты поиска (данные берутся с сайта pkk.rosreestr.ru): ", result)
